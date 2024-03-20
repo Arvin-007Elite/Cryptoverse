d5616afde416b3edafef9289aafe7999262035c7
Arvin <arvinkritik2@gmail.com>
2024-03-20 14:22:44 +0530

    update

--- src/app/store.js
+++ src/app/store.js
@@ -1,22 +1,9 @@
-/* eslint-disable no-undef */
 
-/* eslint-disable no-unused-vars */
-// import { configureStore } from "@reduxjs/toolkit";
 
-// import { cryptoApi } from "../services/cryptoApi";
-// import { cryptoNewsApi } from "../services/cryptoNewsApi";
-
-// export default configureStore({
-//   reducer: {
-//     [cryptoApi.reducerPath]: cryptoApi.reducer,
-//     [cryptoNewsApi.reducerPath]: cryptoNewsApi.reducer,
-//   },
-// });
 import { api } from "../services/cryptoApi";
 import { configureStore } from "@reduxjs/toolkit";
 
 import { cryptoNewsApi } from "../services/cryptoNewsApi";
-
 
 export default configureStore({
   reducer: {
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,3 +1,15 @@
+// import React from "react";
+// import { Spin } from "antd";
+
+// const Loader = () => (
+//   <div className="loader">
+//     <Spin />
+//   </div>
+// );
+
+// export default Loader;
+
+
 import React from "react";
 import { Spin } from "antd";
 
